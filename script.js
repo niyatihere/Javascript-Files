40967ee405c0acb851e0b9db1abae041216c518c
niyatihere <niyutodanakar@gmail.com>
2024-07-16 19:23:31 +0530

    objects in array

---
+++ script.js
@@ -0,0 +1,33 @@
+let student = [
+    {
+        name: 'alice',
+        age: 12,
+        standard: 6,
+    },
+    {
+        name: 'jonny',
+        age: 16,
+        standard: 10,
+    },
+    {
+        name: 'nancy',
+        age: 15,
+        standard: 9,
+    },
+    {
+        name: 'tom',
+        age: 6,
+        standard: 1,
+    },
+    {
+        name: 'scarlet',
+        age: 10,
+        standard: 4,
+    },
+]
+
+for(let i = 0; i < student.length; i++)
+{
+    stu = student[i]
+    console.log(stu.name);
+}
